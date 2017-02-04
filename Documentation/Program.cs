254ad83abf04d9110aacc77299356cd4554f3d99
Unknown6656 <unknown84@gmx.de>
2017-02-04 19:23:04 +0100

    Project system update

---
+++ Documentation/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Reflection;
+using System.Linq;
+using System.Text;
+using System;
+
+namespace Documentation
+{
+    public static class Program
+    {
+        public static int Main(string[] args)
+        {
+            try
+            {
+                Assembly asm = Assembly.LoadFrom(args[0]);
+
+                return 0;
+            }
+            catch
+            {
+                return -1;
+            }
+        }
+    }
+}
---
+++ Documentation/readme.md
@@ -0,0 +1,4 @@
+## This is _not_ the actual See# documentation - this folder only contains the _documentation generator_  
+  
+  
+_Nothing to see here. Move along._
